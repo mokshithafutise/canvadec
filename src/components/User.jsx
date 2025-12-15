76641a80dfe599bb7ecea1c5ef600c4a4b16d61e
dushyant kumar <dushyantvelar@gmail.com>
2025-12-15 16:56:27 +0530

    initial setup

---
+++ src/components/User.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const User = () => {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <p></p>
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default User;
